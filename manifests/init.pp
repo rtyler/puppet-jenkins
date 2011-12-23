04fdf66c1126c82afca2002619cc43c5970f6942
Rafael Felix Correa <rafael.correa@abril.com.br>
2011-12-23 17:19:25 -0200

    removing the #8040 workaround

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,21 +35,12 @@ class jenkins::package {
 }
 
 class jenkins::repo {
-  # JJM These anchors work around #8040
-  #anchor { 'jenkins::repo::alpha': }
-  #anchor { 'jenkins::repo::omega': }
   case $operatingsystem {
     centos, redhat, oel: {
-      class { 'jenkins::repo::el':
-        require => Anchor['jenkins::repo::alpha'],
-        before  => Anchor['jenkins::repo::omega'],
-      }
+      include jenkins::repo::el
     }
     default: {
-      class { 'jenkins::repo::debian':
-        require => Anchor['jenkins::repo::alpha'],
-        before  => Anchor['jenkins::repo::omega'],
-      }
+      include jenkins::repo::debian
     }
   }
 }
