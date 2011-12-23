6984308fce3996ea078428c02bb6c18bc55da340
Rafael Felix Correa <rafael.correa@abril.com.br>
2011-12-23 17:25:10 -0200

    ensuring the jenkins::package session is needed for the service jenkins::servive to run

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,6 +20,7 @@ class jenkins::service {
         enable     => true,
         hasstatus  => true,
         hasrestart => true,
+		require => Package['jenkins::package'],
       }
     }
     # Stay as a no-op to preserve previous behavior
