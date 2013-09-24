ed12a0491f670b2bb57af66288ac93f83f0f2e4f
Benoît Suttor <bsuttor@cirb.irisnet.be>
2013-09-24 16:40:55 +0200

    clean files, move to profile

--- files/upload_to_pulp_testing.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pulp-admin -u jenkins -p j3nk1n5 rpm repo uploads rpm --repo-id testing_${1} -d $WORKSPACE/build/rpm_root/RPMS/x86_64/
-pulp-admin -u jenkins -p j3nk1n5 rpm repo publish run --repo-id testing_${1}
