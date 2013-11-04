80fcab651a7f0b0ea1d8312275ead2e9df814c2d
Jean-François Roche <jfroche@affinitic.be>
2013-11-04 14:29:49 +0100

    CICD-120 puppetize config file

---
+++ manifests/config.pp
@@ -0,0 +1,16 @@
+class jenkins::config (
+  $home='/var/lib/jenkins',
+  $user='jenkins',
+  $port='8080'
+)
+{
+
+  file { '/etc/sysconfig/jenkins':
+    content => template('jenkins/jenkins.erb'),
+    owner   => 'root',
+    group   => 'root',
+    require => Package['jenkins'],
+    notify  => Service['jenkins']
+  }
+
+}
